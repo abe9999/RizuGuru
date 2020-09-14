79cafc0fce3efb03f531d4f5b448e36cb4e9b8b6
Ryosuke Kishi <ryosuke_kishi@skynext.co.jp>
2020-09-14 17:35:51 +0900

    一覧画面でAzureStorageから画像を取得する方法に変更

--- src/components/RestaurantList/ListDetail/ImageBox.vue
+++ src/components/RestaurantList/ListDetail/ImageBox.vue
@@ -1,12 +1,17 @@
 <template>
   <b-col class="image-box">
     <!-- <img :src="require(`@/assets/images/RestaurantList/${imageUrl}`)"> -->
-    <img :src="require(`@/assets/images/RestaurantList/${imageUrl}-1.jpg`)">
+    <img :src="this.url" />
   </b-col>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      url: `https://strizuguru.blob.core.windows.net/image/RestaurantList/${this.imageUrl}-1.jpg`,
+    };
+  },
   props: {
     imageUrl: String,
   },
