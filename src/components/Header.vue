960e2219d5a2da6716f3a86c367f45a46067efba
abe <70552526+abe9999@users.noreply.github.com>
2020-09-18 17:21:51 +0900

    ヘッダーを変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,8 +8,8 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <!-- <b-navbar-nav>
-          <b-nav-item>
+        <b-navbar-nav>
+          <!-- <b-nav-item>
             <router-link to="/" class="nav-link text-light">Home</router-link>
           </b-nav-item>
           <b-nav-item>
@@ -20,16 +20,19 @@
           </b-nav-item>
           <b-nav-item>
             <router-link to="/Restaurant/Registration" class="nav-link text-light">店舗追加</router-link>
+          </b-nav-item>-->
+          <b-nav-item>
+            <router-link to="/RestaurantList/CurrentLocation" class="nav-link text-light">現在地から探す</router-link>
           </b-nav-item>
           <b-nav-item>
             <router-link to="/SearchMap" class="nav-link text-light">マップから検索</router-link>
           </b-nav-item>
-          <b-nav-item>
+          <!-- <b-nav-item>
             <router-link to="/Login" class="nav-link text-light">ログイン</router-link>
-          </b-nav-item>
-        </b-navbar-nav> -->
+          </b-nav-item>-->
+        </b-navbar-nav>
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto"></b-navbar-nav>
+        <!-- <b-navbar-nav class="ml-auto"></b-navbar-nav> -->
       </b-collapse>
     </b-navbar>
   </header>
