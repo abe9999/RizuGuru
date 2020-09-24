f0294b649a0258caba73ee4cd7a3fb6a3feba119
abe <70552526+abe9999@users.noreply.github.com>
2020-09-24 15:43:11 +0900

    地図取得の方法を変更

--- src/components/RestaurantDetail/Map.vue
+++ src/components/RestaurantDetail/Map.vue
@@ -19,73 +19,28 @@ export default {
   components: {
     Category,
   },
-  methods: {
-    loadJS(src) {
-      const ref = window.document.getElementsByTagName("script")[0];
-      const script = window.document.createElement("script");
-      script.src = src;
-      script.async = true;
-      script.setAttribute("id", "google-map");
-      ref.parentNode.insertBefore(script, ref);
-    },
-    initMap() {
-      var map;
-      var marker;
+  mounted() {
+    var map;
+    var marker;
 
-      // axios
-      //   .get(`https://func-rizuguru.azurewebsites.net/api/GetDetail?id=${this.$route.params.id}`)
-      //   .then((res) => {
-      //     map = new google.maps.Map(document.getElementById("map"), {
-      //       gestureHandling: "greedy",
-      //       center: new google.maps.LatLng(
-      //         res.data.latitude,
-      //         res.data.longitude
-      //       ),
-      //       zoom: 16,
-      //     });
+    setTimeout(() => {
+      map = new google.maps.Map(document.getElementById("map"), {
+        gestureHandling: "greedy",
+        center: new google.maps.LatLng(
+          this.detail.latitude,
+          this.detail.longitude
+        ),
+        zoom: 16,
+      });
 
-      //     marker = new google.maps.Marker({
-      //       position: new google.maps.LatLng(
-      //         res.data.latitude,
-      //         res.data.longitude
-      //       ),
-      //       map: map,
-      //     });
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //   });
-
-      setTimeout(() => {
-        map = new google.maps.Map(document.getElementById("map"), {
-          gestureHandling: "greedy",
-          center: new google.maps.LatLng(
-            this.detail.latitude,
-            this.detail.longitude
-          ),
-          zoom: 16,
-        });
-
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(
-            this.detail.latitude,
-            this.detail.longitude
-          ),
-          map: map,
-        });
-      }, 1000);
-    },
-  },
-  created() {
-    this.loadJS(
-      `https://maps.googleapis.com/maps/api/js?key=AIzaSyBoh8D4HaNFoW-IzGEm-Lsx5zqPEhdJ398&callback=initMap`
-    );
-    window.initMap = this.initMap;
-  },
-  destroyed() {
-    const oldTag = document.getElementById("google-map");
-    oldTag.parentNode.removeChild(oldTag);
-    window.google = {};
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(
+          this.detail.latitude,
+          this.detail.longitude
+        ),
+        map: map,
+      });
+    }, 1000);
   },
 };
 </script>
--- src/views/SearchMap/SearchMap.vue
+++ src/views/SearchMap/SearchMap.vue
@@ -8,105 +8,179 @@ import axios from "axios";
 export default {
   /* eslint-disable */
   methods: {
-    loadJS(src) {
-      const ref = window.document.getElementsByTagName("script")[0];
-      const script = window.document.createElement("script");
-      script.src = src;
-      script.async = true;
-      script.setAttribute("id", "google-map");
-      ref.parentNode.insertBefore(script, ref);
-    },
-    initMap() {
-      var latLng;
-      var map;
-      var marker;
-      var markers = [];
-      var infowindow;
+    // loadJS(src) {
+    //   const ref = window.document.getElementsByTagName("script")[0];
+    //   const script = window.document.createElement("script");
+    //   script.src = src;
+    //   script.async = true;
+    //   script.setAttribute("id", "google-map");
+    //   ref.parentNode.insertBefore(script, ref);
+    // },
+    // initMap() {
+    //   var latLng;
+    //   var map;
+    //   var marker;
+    //   var markers = [];
+    //   var infowindow;
+    //   if (navigator.geolocation) {
+    //     // 現在地の取得
+    //     navigator.geolocation.getCurrentPosition(
+    //       function (position) {
+    //         // 緯度経度の取得
+    //         latLng = new google.maps.LatLng(
+    //           position.coords.latitude,
+    //           position.coords.longitude
+    //         );
+    //         // 地図の作成
+    //         map = new google.maps.Map(document.getElementById("map"), {
+    //           gestureHandling: "greedy",
+    //           center: latLng,
+    //           zoom: 14,
+    //         });
+    //         // マーカーの追加
+    //         marker = new google.maps.Marker({
+    //           position: latLng,
+    //           map: map,
+    //         });
+    //         axios
+    //           .get("https://func-rizuguru.azurewebsites.net/api/GetAllDetail")
+    //           .then((res) => {
+    //             for (var i = 0; i < res.data.length; i++) {
+    //               markers[i] = new google.maps.Marker({
+    //                 map: map,
+    //                 id: res.data[i].id,
+    //                 position: new google.maps.LatLng(
+    //                   res.data[i].latitude,
+    //                   res.data[i].longitude
+    //                 ),
+    //                 icon: {
+    //                   url: require("@/assets/images/ロゴ2.png"),
+    //                   scaledSize: new google.maps.Size(35, 35),
+    //                 },
+    //                 title: res.data[i].name,
+    //                 animation: google.maps.Animation.DROP,
+    //               });
+    //             }
+    //           })
+    //           .catch((err) => {
+    //             console.log(err);
+    //           });
+    //         google.maps.event.addListener(map, "idle", function () {
+    //           for (var i = 0; i < markers.length; i++) {
+    //             markerInfo(markers[i], markers[i].title);
+    //           }
+    //         });
+    //         function markerInfo(marker, name) {
+    //           google.maps.event.addListener(marker, "click", function (event) {
+    //             if (infowindow) {
+    //               infowindow.close();
+    //             }
+    //             infowindow = new google.maps.InfoWindow({
+    //               content: `<a href="#/Restaurant/Detail/${marker.id}">${marker.title}</a>`,
+    //             });
+    //             infowindow.open(marker.getMap(), marker);
+    //           });
+    //         }
+    //       },
+    //       function () {
+    //         alert("位置情報取得に失敗しました");
+    //       }
+    //     );
+    //   } else {
+    //     alert("この端末では位置情報が取得できません");
+    //   }
+    // },
+  },
+  // created() {
+  //   this.loadJS(
+  //     `https://maps.googleapis.com/maps/api/js?key=AIzaSyBoh8D4HaNFoW-IzGEm-Lsx5zqPEhdJ398&callback=initMap`
+  //   );
+  //   window.initMap = this.initMap;
+  // },
+  // destroyed() {
+  //   const oldTag = document.getElementById("google-map");
+  //   oldTag.parentNode.removeChild(oldTag);
+  //   window.google = {};
+  // },
+  mounted() {
+    var latLng;
+    var map;
+    var marker;
+    var markers = [];
+    var infowindow;
 
-      if (navigator.geolocation) {
-        // 現在地の取得
-        navigator.geolocation.getCurrentPosition(
-          function (position) {
-            // 緯度経度の取得
-            latLng = new google.maps.LatLng(
-              position.coords.latitude,
-              position.coords.longitude
-            );
+    if (navigator.geolocation) {
+      // 現在地の取得
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          // 緯度経度の取得
+          latLng = new google.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          );
 
-            // 地図の作成
-            map = new google.maps.Map(document.getElementById("map"), {
-              gestureHandling: "greedy",
-              center: latLng,
-              zoom: 14,
-            });
+          // 地図の作成
+          map = new google.maps.Map(document.getElementById("map"), {
+            gestureHandling: "greedy",
+            center: latLng,
+            zoom: 14,
+          });
 
-            // マーカーの追加
-            marker = new google.maps.Marker({
-              position: latLng,
-              map: map,
-            });
+          // マーカーの追加
+          marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+          });
 
-            axios
-              .get("https://func-rizuguru.azurewebsites.net/api/GetAllDetail")
-              .then((res) => {
-                for (var i = 0; i < res.data.length; i++) {
-                  markers[i] = new google.maps.Marker({
-                    map: map,
-                    id: res.data[i].id,
-                    position: new google.maps.LatLng(
-                      res.data[i].latitude,
-                      res.data[i].longitude
-                    ),
-                    icon: {
-                      url: require("@/assets/images/ロゴ2.png"),
-                      scaledSize: new google.maps.Size(35, 35),
-                    },
-                    title: res.data[i].name,
-                    animation: google.maps.Animation.DROP,
-                  });
-                }
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-
-            google.maps.event.addListener(map, "idle", function () {
-              for (var i = 0; i < markers.length; i++) {
-                markerInfo(markers[i], markers[i].title);
-              }
-            });
-
-            function markerInfo(marker, name) {
-              google.maps.event.addListener(marker, "click", function (event) {
-                if (infowindow) {
-                  infowindow.close();
-                }
-                infowindow = new google.maps.InfoWindow({
-                  content: `<a href="#/Restaurant/Detail/${marker.id}">${marker.title}</a>`,
+          axios
+            .get("https://func-rizuguru.azurewebsites.net/api/GetAllDetail")
+            .then((res) => {
+              for (var i = 0; i < res.data.length; i++) {
+                markers[i] = new google.maps.Marker({
+                  map: map,
+                  id: res.data[i].id,
+                  position: new google.maps.LatLng(
+                    res.data[i].latitude,
+                    res.data[i].longitude
+                  ),
+                  icon: {
+                    url: require("@/assets/images/ロゴ2.png"),
+                    scaledSize: new google.maps.Size(35, 35),
+                  },
+                  title: res.data[i].name,
+                  animation: google.maps.Animation.DROP,
                 });
-                infowindow.open(marker.getMap(), marker);
-              });
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+
+          google.maps.event.addListener(map, "idle", function () {
+            for (var i = 0; i < markers.length; i++) {
+              markerInfo(markers[i], markers[i].title);
             }
-          },
-          function () {
-            alert("位置情報取得に失敗しました");
+          });
+
+          function markerInfo(marker, name) {
+            google.maps.event.addListener(marker, "click", function (event) {
+              if (infowindow) {
+                infowindow.close();
+              }
+              infowindow = new google.maps.InfoWindow({
+                content: `<a href="#/Restaurant/Detail/${marker.id}">${marker.title}</a>`,
+              });
+              infowindow.open(marker.getMap(), marker);
+            });
           }
-        );
-      } else {
-        alert("この端末では位置情報が取得できません");
-      }
-    },
-  },
-  created() {
-    this.loadJS(
-      `https://maps.googleapis.com/maps/api/js?key=AIzaSyBoh8D4HaNFoW-IzGEm-Lsx5zqPEhdJ398&callback=initMap`
-    );
-    window.initMap = this.initMap;
-  },
-  destroyed() {
-    const oldTag = document.getElementById("google-map");
-    oldTag.parentNode.removeChild(oldTag);
-    window.google = {};
+        },
+        function () {
+          alert("位置情報取得に失敗しました");
+        }
+      );
+    } else {
+      alert("この端末では位置情報が取得できません");
+    }
   },
 };
 </script>
