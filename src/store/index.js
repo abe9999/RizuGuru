abe8cc5e7a07c350773be710ad8f12dd5c4731ff
Ryosuke Kishi <ryosuke_kishi@skynext.co.jp>
2020-10-07 08:32:02 +0900

    リロード後もログイン状態が維持されるように修正

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate"
-import Filtering from "./modules/RestaurantList/Filtering.js";
-import UserData from "./modules/UserData.js";
-import Location from "./modules/Location.js";
-import Search from "./modules/Search.js";
 import Auth from "./modules/Auth.js"
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   modules: {
-    Filtering,
-    UserData,
-    Location,
-    Search,
     Auth
   },
   strict: true,
   plugins: [createPersistedState({
     key: "RizuGuru",
-    paths: ["Location.lat", "Location.lng"],
+    paths: ["Auth.isAuthed"],
     storage: window.sessionStorage
   })]
 });
