51fb7a86d57ab358bd06be37771a6ab1ac522f68
Ryosuke Kishi <ryosuke_kishi@skynext.co.jp>
2020-10-07 15:57:10 +0900

    leaveGuardを修正

--- src/plugins/leaveGuard.js
+++ src/plugins/leaveGuard.js
@@ -1,8 +1,9 @@
-const completeViewPathList = ["/RestaurantRegistration/Complete", "/MenuRegistration/Complete"]
+const completeViewPathList = ["/RestaurantRegistration/Complete", "/MenuRegistration/Complete/"]
 export const leaveGuard = {
     beforeRouteLeave(to, from, next) {
+        console.log(completeViewPathList.indexOf(to.path) == -1)
         // completeViewPathListに含まれていないパスに遷移する場合、アラートを表示する。
-        if (completeViewPathList.indexOf(to.path)) {
+        if (completeViewPathList.indexOf(to.path) == -1) {
             const answer = window.confirm('入力した情報が消去されます。よろしいですか？');
             if (answer) {
                 next();
