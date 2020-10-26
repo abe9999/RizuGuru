28bda659611df09bd81d73d7ba0172fb60dde051
Ryosuke Kishi <ryosuke_kishi@skynext.co.jp>
2020-10-26 14:41:41 +0900

    プラグインを修正

--- src/plugins/searchRestaurantList.js
+++ src/plugins/searchRestaurantList.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export async function searchRestaurantList({ searchQuery, offset = 0 }) {
-    console.log(`https://func-rizuguru.azurewebsites.net/api/SearchRestaurantList?offset=${offset}&keyword=${searchQuery.keyword}&lat=${searchQuery.lat}&lng=${searchQuery.lng}&station=${searchQuery.station}&genre=${searchQuery.genre}&distance=${searchQuery.distance}&minPrice=${searchQuery.minPrice}&maxPrice=${searchQuery.maxPrice}&tagId=${searchQuery.tagsId}`)
     return new Promise((resolve) => {
         axios.get(`
         https://func-rizuguru.azurewebsites.net/api/SearchRestaurantList?offset=${offset}&keyword=${searchQuery.keyword}&lat=${searchQuery.lat}&lng=${searchQuery.lng}&station=${searchQuery.station}&genre=${searchQuery.genre}&distance=${searchQuery.distance}&minPrice=${searchQuery.minPrice}&maxPrice=${searchQuery.maxPrice}&tagId=${searchQuery.tagsId}`)
