46e5050cb0693867703102e7a9e8cc781c9e83e4
Ryosuke Kishi <ryosuke_kishi@skynext.co.jp>
2020-10-29 17:42:25 +0900

    コンポーネントを追加

---
+++ src/components/Molecules/Filtering/PredictionConversionFormUnit.vue
@@ -0,0 +1,132 @@
+<template>
+  <section class="wrapper">
+    <LabelWithIcon
+      class="label"
+      :title="itemData.displayName"
+      :icon="itemData.icon"
+    />
+    <section v-if="selectedWord !== ''" @click="deleteSelectedWord">
+      <p class="selected-word">{{ selectedWord }}<b-icon icon="x" /></p>
+    </section>
+    <section v-if="selectedWord === ''" class="prediction-wrapper">
+      <InputForm
+        :getter="getInputWord"
+        :setter="setInputWord"
+        :debounce="500"
+        :propertyName="itemData.propertyName"
+        :required="itemData.required"
+        :placeholder="itemData.placeholder"
+        :validationSetter="itemData.validationSetter"
+      />
+      <section v-if="predictionList.length">
+        <p class="prediction-information">{{ predictionInformation }}</p>
+        <p
+          class="prediction-word"
+          v-for="predictionWord in predictionList"
+          :key="predictionWord.id"
+          @click="selectSelectedWord(predictionWord)"
+        >
+          {{ predictionWord.name }}
+        </p>
+      </section>
+    </section>
+  </section>
+</template>
+
+<script>
+import LabelWithIcon from "@/components/Atoms/LabelWithIcon.vue";
+import InputForm from "@/components/Atoms/InputForm.vue";
+
+export default {
+  components: {
+    LabelWithIcon,
+    InputForm,
+  },
+  props: {
+    itemData: Object,
+    setter: Function,
+    api: Function,
+    predictionInformation: String,
+  },
+  data() {
+    return {
+      inputWord: "",
+      selectedWord: "",
+      predictionList: [],
+      loading: false,
+    };
+  },
+  methods: {
+    getInputWord() {
+      return this.inputWord;
+    },
+    setInputWord({ value }) {
+      this.inputWord = value;
+      this.predictionList = [];
+      if (value !== "") {
+        this.api(value).then((res) => {
+          this.predictionList.push(...res.slice(0, 20));
+        });
+      }
+    },
+    selectSelectedWord(predictionWord) {
+      this.selectedWord = predictionWord.name;
+    },
+    deleteSelectedWord() {
+      this.selectedWord = "";
+    },
+  },
+  watch: {
+    selectedWord(word) {
+      this.setter({ propertyName: this.itemData.propertyName, value: word });
+    },
+  },
+  mounted() {
+    this.selectedWord = this.itemData.value;
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+}
+
+.label {
+  margin-bottom: 25px;
+}
+
+.selected-word {
+  display: inline-block;
+  padding: 5px;
+  margin: 2.5px 20px;
+  font-size: 15px;
+  cursor: pointer;
+  color: white;
+  background-color: #689e39;
+  border-radius: 2.5px;
+}
+
+.selected-word:hover {
+  opacity: 0.5;
+  text-decoration: line-through;
+}
+
+.prediction-information {
+  margin: 0;
+}
+
+.prediction-word {
+  display: inline-block;
+  padding: 5px;
+  margin: 2.5px;
+  cursor: pointer;
+  background-color: #f6f6f6;
+  border-radius: 2.5px;
+}
+
+.prediction-word:hover {
+  color: white;
+  background-color: gray;
+}
+</style>
